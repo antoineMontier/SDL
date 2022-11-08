e5bb67d9cf1a75a88a53f1df078b3b78fce56b86
Antoine Montier <115585369+antoineMontier@users.noreply.github.com>
2022-11-08 20:29:37 +0100

    First use of sdl
    display the sdl version

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <SDL.h>
+#include <stdio.h>
+
+int main(int argc, char argv){
+
+    SDL_version nb;
+    SDL_VERSION(&nb);
+
+    printf("SDL %d.%d.%d !\n", nb.major, nb.minor, nb.patch);
+    return 0;
+
+
+}
