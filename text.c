a30791278b631d929b4bfc908d770b32281740a6
Antoine Montier <115585369+antoineMontier@users.noreply.github.com>
2022-11-15 11:26:34 +0100

    Closing font makes a segmentation error ...:/

--- text.c
+++ text.c
@@ -9,16 +9,27 @@
 
 void SDL_ExitWithError(const char *string);
 
-int main(int argc, char *argv[]){//compile with     gcc text.c -o text -lm -lSDL_ttf $(sdl2-config --cflags --libs) && ./text
+int main(int argc, char *argv[]){//compile with     gcc text.c -o text -lm -lSDL2_ttf $(sdl2-config --cflags --libs) && ./text
     printf("\n\n");
     SDL_Window *w;//open a window command
     printf("window opened ...\n");
+
     SDL_Renderer *ren;//render creation
     printf("renderer opened ...\n");
+
+    if(TTF_Init() != 0)
+        SDL_ExitWithError("Initialisation TTF failed");
+    else
+        printf("TTF opened...\n");
+
     TTF_Font* font;
     printf("font opened...\n");
-    font = TTF_OpenFont("../Roboto/Roboto-Black.ttf", 12);
-    printf("font assigned...\n");
+
+    font = TTF_OpenFont("Roboto-Regular.ttf", 20);
+    if(font != 0)
+        printf("font assigned...\n");
+    else
+        SDL_ExitWithError("Initialisation font failed");
 
     if(0 != SDL_Init(/*flag*/ SDL_INIT_VIDEO))//lots of flags like SDL_INIT_AUDIO ; *_VIDEO ; *_EVERYTHING... To separe with '|'
         SDL_ExitWithError("Initialisation SDL failed");
@@ -33,15 +44,16 @@ int main(int argc, char *argv[]){//compile with     gcc text.c -o text -lm -lSDL
         printf("renderer and window created...\n");
 
 //===================================================================================================================================================
-    if(TTF_Init() != 0)
-        SDL_ExitWithError("Initialisation TTF failed");
-    else
-        printf("TTF opened...\n");
+    
 
 
+        SDL_Color c = {255, 0, 0};//red
+        printf("color assigned..\n");
 
 
-
+        SDL_Surface* surface = TTF_RenderText_Solid(font, "Hello world !", c);
+        if(surface == NULL)
+            SDL_ExitWithError("Initialisation surface failed");
 
 
 
@@ -65,14 +77,24 @@ int main(int argc, char *argv[]){//compile with     gcc text.c -o text -lm -lSDL
 
 //===================================================================================================================================================
 
+
+    SDL_FreeSurface(surface);
+    printf("surface quitted..\n");
+
     TTF_Quit();
     printf("quitted TTF...\n");
-    TTF_CloseFont(font);
-    printf("closed font...\n");
+    if(font){
+        printf("closing font %p...\n", font);
+        ///TTF_CloseFont(font);
+        ///printf("closed font...\n");
+    }
+
     SDL_DestroyRenderer(ren);
     printf("renderer destroyed...\n");
+
     SDL_DestroyWindow(w);
     printf("window destroyed...\n");
+
     SDL_Quit();
     printf("SDL quitted");
 
