9d85c57f0288cfda340716d80917e8d711e17f1d
Antoine Montier <115585369+antoineMontier@users.noreply.github.com>
2022-11-15 10:36:08 +0100

    First commit of trying to display text...

---
+++ text.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define HEIGHT 720
+#define WIDTH 1080
+
+
+
+int main(int argc, char *argv[]){//compile with     gcc text.c -o text -lm -lSDL_ttf $(sdl2-config --cflags --libs) && ./text
+    printf("\n\n");
+
+
+
+
+
+    printf("Hello\n");
+
+
+
+
+
+
+
+
+
+    printf("\n\n");
+    return 0;
+}
+
+
+
+
+
+
+
+
