046e209c89729181ee45a36a296740910c6a5d60
Antoine Montier <115585369+antoineMontier@users.noreply.github.com>
2022-12-16 11:21:31 +0100

    resizable window

--- main.c
+++ main.c
@@ -2,104 +2,75 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int main(int argc, char *argv[])
+{ // compile and execute with     gcc main.c -o main -lm -lSDL2_ttf $(sdl2-config --cflags --libs) && ./main
 
-
-
-
-
-
-
-
-int main(int argc, char *argv[]){//compile and execute with     gcc main.c -o main -lm -lSDL2_ttf $(sdl2-config --cflags --libs) && ./main
-
-    SDL_Window *w;//open a window command
-    SDL_Renderer *ren;//render creation
-
-    openSDL(WIDTH, HEIGHT, 0, &w, &ren);
-
-
+    SDL_Window *win;   // open a window command
+    SDL_Renderer *ren; // render creation
+    openSDL(WIDTH, HEIGHT, 0, "name test", &win, &ren);
+    SDL_SetWindowResizable(win, SDL_TRUE);
     /*----------------------------------------------------------------------------------------------------------------------------------------------------------------*/
-    SDL_bool program_launched = SDL_TRUE; //SDL_FALSE or SDL_TRUE
+    SDL_bool program_launched = SDL_TRUE; // SDL_FALSE or SDL_TRUE
 
-
-    TTF_Font * font;
+    TTF_Font *font;
     setFont(&font, "a.ttf", 50);
+    double x = 150, y = 300, w = 150, h = 200, tl = 50, tr = 10, br = 40, bl = 1;
 
-
-    while(program_launched){//principal loop
+    while (program_launched)
+    { // principal loop
         SDL_Event evt;
 
-            background(ren, 128, 64, 0, WIDTH, HEIGHT);
+        color(ren, 0, 200, 0, 1);
+        rect(ren, 20, 20, 500, 400, 1);
+        for(double a = 0 ; a <= 255 ; a += 1){
+            color(ren, 0, 20, 200, a);
+            mark(ren, 50 + a, 100,1 );
 
-    
-            color(ren, 255, 0, 0, 255);//red
-            roundRect(ren, 100, 100, 50, 120, 1, 20);
-
-            circle(ren, 200, 600, 80, 1);
-            roundRect(ren, 400, 600, 50, 80, 0, 10);
-
-            text(ren, 30, 30, "test", font, 162, 30, 200);
-
-            color(ren, 0, 255, 0, 255);//green
-            roundRect(ren, 300, 300, 200, 100, 1, 20);
-            roundRect(ren, 100, 300, 80, 40, 0, 10);
-
-            circle(ren, 150, 500, 50, 0);
+        }
+        
 
 
 
-        SDL_RenderPresent(ren);//refresh the render
+        SDL_Delay(100);
 
+        SDL_RenderPresent(ren); // refresh the render
 
+        while (SDL_PollEvent(&evt))
+        { // reads all the events (mouse moving, key pressed...)        //possible to wait for an event with SDL_WaitEvent
+            switch (evt.type)
+            {
+            case SDL_WINDOWEVENT:
+                if (evt.window.event == SDL_WINDOWEVENT_RESIZED) {
+                    printf("MESSAGE:Resizing window...\n");
+                    //resizeWindow(evt.window.data1, evt. window.data2);
+                }
+                break;
+            case SDL_QUIT:
+                program_launched = SDL_FALSE; // quit the program if the user closes the window
+                break;
 
-        while(SDL_PollEvent(&evt)){//reads all the events (mouse moving, key pressed...)        //possible to wait for an event with SDL_WaitEvent
-            switch(evt.type){
+            case SDL_KEYDOWN: // SDL_KEYDOWN : hold a key            SDL_KEYUP : release a key
+                switch (evt.key.keysym.sym)
+                { // returns the key ('0' ; 'e' ; 'SPACE'...)
 
-                case SDL_QUIT:
-                    program_launched = SDL_FALSE;//quit the program if the user closes the window
+                case SDLK_ESCAPE:
+                    program_launched = SDL_FALSE; // escape the program by pressing esc
                     break;
 
-                case SDL_KEYDOWN:                   //SDL_KEYDOWN : hold a key            SDL_KEYUP : release a key
-                    switch (evt.key.keysym.sym){//returns the key ('0' ; 'e' ; 'SPACE'...)
-
-                        case SDLK_ESCAPE:
-                            program_launched = SDL_FALSE;//escape the program by pressing esc
-                            break;
-
-
-
-                        default:
-                            break;
-                    }
-
-
-
                 default:
-                    break; 
-
+                    break;
+                }
+            default:
+                break;
             }
         }
-
-        
-
-    
-        
-
-
-    
-
-
     }
-
-
-
 
     /*SDL_RenderPresent(ren);//refresh the render
     SDL_Delay(5000);//waiting delay, in ms*/
     /*----------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
-    
-    closeSDL(&w, &ren);
+    closeSDL(&win, &ren);
     printf("closed successfully !\n");
     return 0;
 }
